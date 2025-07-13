f1f7a56293e028d54f88b636e028a53ba7eb1664
LuvSharma007 <luvsharma9910@gmail.com>
2025-07-13 11:16:47 +0530

    merge sorted array & buy and sell stock

--- Month 1/DSA/02-Array-level-0.js
+++ Month 1/DSA/02-Array-level-0.js
@@ -210,6 +210,60 @@
 
 // console.log(reverseString(arr));
 
+// 11.) best time to but and sell stocks
+
+// let arr = [7,1,5,3,6,4];
+// let left = 0 , right = 1 , maxProfit = 0;
+// // buying day  // selling day
+// const MaxProfit = (arr)=>{
+//     while(right<arr.length){
+//         if(arr[right] > arr[left]){
+//             let profit = arr[right] - arr[left];
+//             maxProfit = Math.max(maxProfit,profit);
+//         }else{
+//             left = right;
+//         }
+//         right++;
+//     }
+//     return maxProfit;
+// }
+
+// console.log(MaxProfit(arr));
+
+
+// 12.) merge Sorted Array
+//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+
+// let MergeSortedArray = () =>{
+//     let i = m-1;
+//     let j = n-1;
+//     let k = m+n-1;
+//     while(i>=0 && j>=0){
+//         if(nums1[i]>nums2[j]){
+//             nums1[k] = nums1[i]
+//             i--
+//         }else{
+//             nums1[k] = nums2[j];
+//             j--
+//         }
+//         k--;
+//     }
+//     while(j>=0){
+//         nums1[k] = nums2[j];
+//         j--;
+//         k--;
+//     }
+
+//     return nums1;
+// }
+
+// console.log(MergeSortedArray());
+
+
+
 
 
 
