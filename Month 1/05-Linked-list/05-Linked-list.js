469881b43e23e0528649db8ef5695cbcf08a7d76
LuvSharma007 <luvsharma9910@gmail.com>
2025-07-23 10:37:06 +0530

    middle element

--- Month 1/05-Linked-list/05-Linked-list.js
+++ Month 1/05-Linked-list/05-Linked-list.js
@@ -39,6 +39,8 @@ class LinkedList{
         }        
     }
 
+    // this handles 3 cases
+
     delete(position){
         let current = this.head;
         let prev = null;
@@ -60,18 +62,48 @@ class LinkedList{
         }
         prev.next = current.next;
     }
+
+    // if the no. of elements are even, there must be two middle , so return second middle
+    // if the no. of elements are odd, return middle
+
+    middleElm(){
+        if(this.head === null){
+            console.log("Linked list is empty");
+            return;
+        }
+        if(this.head.next === null){
+            console.log("Only one elements in the linked list so return it");
+            return this.head.value
+        }
+        let slow_ptr = this.head;
+        let fast_ptr = this.head
+        while(fast_ptr!==null && fast_ptr.next!==null){
+            fast_ptr = fast_ptr.next.next;
+            slow_ptr = slow_ptr.next;
+        }
+        return slow_ptr.value;
+    }
 }
 
 const LL = new LinkedList()
+LL.insert(1);
+LL.insert(2);
+LL.insert(3);
+LL.insert(4);
 LL.insert(5);
 LL.insert(6);
 LL.insert(7);
+LL.insert(8);
 
-LL.printList();
-console.log("----------------------------After deletion");
+// LL.printList();
+// console.log("After deletion");
 
-LL.delete(2);
-LL.printList();
+// LL.delete(2);
+// LL.printList();
+
+console.log(LL.middleElm());
+
+
 
 
 
